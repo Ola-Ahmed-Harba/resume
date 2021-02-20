8812961f3da5b3a5eae4eac9e92280515e3a6196
Ola Ahmed Harba <39811703+Ola-Ahmed-Harba@users.noreply.github.com>
2021-02-20 17:57:49 +0200

    Create resume.py

---
+++ resume.py
@@ -0,0 +1,37 @@
+import life
+
+
+
+class Ola_m(life):
+      def __init__(self,Nature ,adjectives):
+          self.Learning
+          self.Responsibilities
+          self.work
+          self.Attempts
+          self.Hope 
+          self.Patience
+          self.Thoughts
+          self.Dreams  [None]
+          self.Areas_of_life = ["*"]
+          self.Motherland = [] #custom life
+          
+      def its_me(self):
+          self.knowledge = {} #Human is simply knowledge
+          
+      def Create_upbringing(self ,name):
+          learn = self.math([name ,Motherland])
+          self.knowledge["my Name"] = None if learn[0] > learn[1] else "i love math"
+          learn = self.Sciences(self.knowledge ,self.Areas_of_life)
+          
+          self.knowledge["Features"] = "you are special" if learn else "you are too little"
+          learn = lambda self.Dreams: [ol if ol in self.Motherland \
+                                          and in self.Areas_of_life \
+                                          else max(self.Hope,self.Reality ,self.Learning) for ol in self.Dreams]
+
+          self.knowledge["our with life"] = learn
+
+          #the output [i Live math but life!..]
+          
+      def New_life(self ,State ,Target):
+          if State in 
+             pass #Nothing is complete
